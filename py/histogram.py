bdee07c0021cddfe8163218991c78d9c65654086
Bartłomiej Jędrzej Moroz <b.moroz@stud.elka.pw.edu.pl>
2023-04-13 18:29:05 +0200

    Entropy WILL prevail

--- py/histogram.py
+++ py/histogram.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import io
 import math
-from typing import Tuple
-
+from typing import Tuple, List
 
 def validate_header(file: io.BufferedReader) -> Tuple[Tuple[int, int], int]:
     """
@@ -37,7 +36,7 @@ def validate_header(file: io.BufferedReader) -> Tuple[Tuple[int, int], int]:
     if not max.isnumeric() or int(max) <= 0:
         raise RuntimeError("File has incorrect maximum value set!")
 
-    return (tuple(dimensions), int(max))
+    return (tuple([int(x) for x in dimensions]), int(max))
 
 
 def read_file(path: str) -> Tuple[np.ndarray, int]:
@@ -45,7 +44,7 @@ def read_file(path: str) -> Tuple[np.ndarray, int]:
     Parse raw binary data into a NumPy array.
 
     :param path: String path to the file.
-    :returns: Pair of NumPy array holding data and maximum possible value.
+    :returns: Tuple of NumPy array holding data, dataset dimensions and maximum possible value.
     """
     data = None
     with open(os.path.dirname(__file__) + "/" + path, "rb") as file:
@@ -54,31 +53,55 @@ def read_file(path: str) -> Tuple[np.ndarray, int]:
         # Continue reading from the file, raw data is unsigned 8 bit integers.
         data = np.fromfile(file, dtype=np.dtype('B'))
 
-    return data, max
+    return data, dimensions, max
 
 
-def process_file(dir: str, name: str, axis: plt.Axes) -> None:
+def calculate_entropy(sums: List[int], total: int) -> float:
     """
-    Given relative path to the file, process its content and draw a histogram.
+    Calculate entropy of dataset processed into a histogram.
+
+    :param sums: Counts of specific sample values.
+    :param total: Total sample count.
+    :returns: Entropy value.
+    """
+    entropy = 0
+    for sum in sums:
+        probability = sum / total
+        if probability != 0:
+            entropy -= probability * math.log2(probability)
+    
+    return entropy
+
+
+def process_file(dir: str, name: str, axis: plt.Axes) -> float:
+    """
+    Given relative path to the file, process its content, draw a histogram and calculate entropy.
 
     :param dir: String path to the directory.
     :param name: String name of the file (also subplot name).
     :param axis: PyPlot axis to draw to.
-    :returns: None.
+    :returns: Calculated entropy of the dataset.
     """
-    data, max = read_file(dir + name)
-    axis.hist(data, bins=max, range=(0, max))
+    data, dimensions, max = read_file(dir + name)
+    # Save bars from the histogram
+    _, _, bars = axis.hist(data, bins=max, range=(0, max))
     axis.set_title(name)
     axis.set_xlim(0, max)
+    # Extract bar values and calculate entropy
+    sums = [bar.get_height() for bar in bars]
+    entropy = calculate_entropy(sums, dimensions[0] * dimensions[1])
+
+    return entropy
 
 
-def process_folder(path: str) -> None:
+def process_folder(path: str) -> List[Tuple[str, float]]:
     """
     Given relative path to a directory, process all files inside and create a rectangular plot grid with histograms.
 
     :param path: String path to the directory.
-    :returns: None.
+    :returns: List of pairs (file name, entropy).
     """
+    entropies = []
     # Obtain a list of all files in a directory.
     files = os.listdir(os.path.dirname(__file__) + "/" + path)
     # Calculate an optimal (or rather, good enough) grid shape for axes.
@@ -88,12 +111,22 @@ def process_folder(path: str) -> None:
     # Process each file sequentially and assign its histogram to an axis.
     for file, i in zip(files, range(len(files))):
         print(f"Processing file #{i + 1}: {file}...")
-        process_file(path, file, axes[i // grid_shape[1]][i % grid_shape[1]])
+        entropy = process_file(path, file, axes[i // grid_shape[1]][i % grid_shape[1]])
+        entropies.append((file, entropy))
+    
+    return entropies
 
 
 if __name__ == "__main__":
+    results = []
     dirs = ["../data/txt/", "../data/img/"]
     for dir in dirs:
         print(f"Processing folder {dir}...")
-        process_folder(dir)
+        entropies = process_folder(dir)
+        results.extend(entropies)
+    
+    print("Entropy values:")
+    for file, entropy in results:
+        print(f"File {file}: {entropy}")
+
     plt.show()
