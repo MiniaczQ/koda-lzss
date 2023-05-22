0c1a1addf9d887cfc629bf4129b38ab51488e75c
Dawid Sygocki <24943032+Dove6@users.noreply.github.com>
2023-05-22 17:59:46 +0200

    Prepare test script

---
+++ test_on_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CODER='./koda-lzss.exe'
+DECODER='python py/decoder.py'
+
+function test_file {
+    echo "Testing file $1..."
+    local coded_name="$1.lzss"
+    local decoded_name="$1"
+    local dir="$(dirname "${coded_name}")"
+    mkdir -p "test/${dir}"
+    echo "  (coding...)"
+    ${CODER} -i "$1" -o "test/${coded_name}"
+    echo "  (decoding...)"
+    ${DECODER} "$1" "test/${decoded_name}"
+    echo "  (diffing...)"
+    diff -s "$1" "test/${decoded_name}"
+    echo ""
+}
+export -f test_file
+export CODER
+export DECODER
+
+find ./data -name \*.pgm -exec bash -c 'test_file "$0"' {} \;
