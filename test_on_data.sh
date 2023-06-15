8b7b248eea296a107584384cf5688fd6ad5ac8c9
Dawid Sygocki <24943032+Dove6@users.noreply.github.com>
2023-06-15 19:53:51 +0200

    [Test] Parametrize test script using reference distance and length bit width

--- test_on_data.sh
+++ test_on_data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-CODER='./target/debug/koda-lzss.exe'
-DECODER='python py/decoder.py'
+DISTANCE_BIT_WIDTH=${1:-8}
+LENGTH_BIT_WIDTH=${2:-8}
+
+CODER="./target/debug/koda-lzss.exe -d ${DISTANCE_BIT_WIDTH} -m ${LENGTH_BIT_WIDTH}"
+DECODER="python py/decoder.py --window-size $((2 ** ${DISTANCE_BIT_WIDTH})) --length-width ${LENGTH_BIT_WIDTH}"
 
 function test_file {
     echo "Testing file $1..."
@@ -10,7 +13,7 @@ function test_file {
     local dir="$(dirname "${coded_name}")"
     mkdir -p "test/${dir}"
     echo "  (coding...)"
-    ${CODER} -i "$1" -o "test/${coded_name}"
+    ${CODER} "$1" "test/${coded_name}"
     echo "  (decoding...)"
     ${DECODER} "test/${coded_name}" "test/${decoded_name}"
     echo "  (diffing...)"
